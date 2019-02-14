4d1941eb224072f5b3d4a557c60d40e394f6dc3a
Elias Ravarotto <elias_rvt@hotmail.com>
2019-02-14 12:04:32 -0300

    Se agrega opctions a los Flashmessages

--- resources/views/frontend/flash-message.blade.php
+++ resources/views/frontend/flash-message.blade.php
@@ -2,24 +2,38 @@
 <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 
 <script>
+    toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "50000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
 
   @if(Session::has('success'))
-  		toastr.success("{{ Session::get('success') }}");
+      toastr.success("{{ Session::get('success') }}");
   @endif
 
-
   @if(Session::has('info'))
-  		toastr.info("{{ Session::get('info') }}");
+      toastr.info("{{ Session::get('info') }}");
   @endif
 
   @if(Session::has('warning'))
-  		toastr.warning("{{ Session::get('warning') }}");
+      toastr.warning("{{ Session::get('warning') }}");
   @endif
-
 
   @if(Session::has('error'))
-  		toastr.error("{{ Session::get('error') }}");
+      toastr.error("{{ Session::get('error') }}");
   @endif
-
 
 </script>
