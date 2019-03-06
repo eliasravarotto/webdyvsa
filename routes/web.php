7390217d5f07ec62b72eef37ef49105733dd291a
Elias Ravarotto <elias_rvt@hotmail.com>
2019-03-06 12:17:30 -0300

    testing curl

--- routes/web.php
+++ routes/web.php
@@ -65,3 +65,56 @@ Route::post('/test-drive','SolicitudTestDriveController@store');
 Route::post('/contacto','MensajeEmailController@store');
 // Route::post('/consultar/usado/{id?}','ConsultaUsadoController@store');
 Route::get('/dia-de-la-mujer','FrontController@publicidad');
+
+
+Route::get('/push', function(){
+	$data = [
+	   [
+	   	"data" => 
+		    	["notification" =>
+		    		["title" => "FCM Message",
+		    		 "body" => "This is an FCM Message",
+		    		 "icon" => "https://www.derkayvargas.com/imagenes/logo-toyota.png",
+		    		]
+		    	],
+	   	"to" => "cAOGvv3cEW8:APA91bF6hb9c_NYORHM4WeQDoQtnUn9yHTOg2aaffWmU1ebw1Jivadk8kbG1x5MZkOO860heqO0WrHp1cwiWvCZ3EYTgUInPY0aW_Jp7R8W-yTXZKKBhn-TPliYqTMBDLJalIJQ7iUF-"
+	   ]
+	];
+
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+	    CURLOPT_URL => "https://fcm.googleapis.com/fcm/send",
+	    CURLOPT_RETURNTRANSFER => true,
+	    CURLOPT_ENCODING => "",
+	    CURLOPT_MAXREDIRS => 10,
+	    CURLOPT_TIMEOUT => 30000,
+	    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	    CURLOPT_CUSTOMREQUEST => "POST",
+	    CURLOPT_POSTFIELDS => json_encode($data),
+	    CURLOPT_HTTPHEADER => array(
+	    	// Set here requred headers
+	        "accept: */*",
+	        "accept-language: en-US,en;q=0.8",
+	        "content-type: application/json",
+	        "Authorization: key=AAAApfZBaXY:APA91bEYw2wELGsLG2WsxFmrEj-xmPvKdpPqv6y8OeTa7mDA3HMSkKZhcJ5tE3uDXSInXReVy-cJvC4f20_7ySbfqZhXCSeLrf5JEccLdQjd3mNkx8mduKjh5Gdg07SZbF4AIGsZ1Db-",
+	    ),
+	));
+
+	$response = curl_exec($curl);
+	$err = curl_error($curl);
+
+	curl_close($curl);
+	 
+
+	if ($err) {
+	    echo "cURL Error #:" . $err;
+	} else {
+	   return json_decode($response);
+	}
+
+	// url will be: http://my.domain.com/test.php?key1=5&key2=ABC;
+
+	//$statusCode = $response->getStatusCode();
+	//$content = $response->getBody();
+});
