a0ede343e8080ed5c373fc0021aec48d3cb37ef0
Elias Ravarotto <elias_rvt@hotmail.com>
2019-03-12 09:37:58 -0300

    UPDATE sorteo dia de la mujer

--- resources/views/frontend/promo.blade.php
+++ resources/views/frontend/promo.blade.php
@@ -15,6 +15,8 @@
 				<li style="font-size: 15px">No podrá participar personal en relación de dependencia con la empresa, ni sus familiares directos hasta el segundo grado (cónyuges, hijas, madres)</li> <br>
 				<li style="font-size: 15px">Se sorteará un bono por $ 3.000,- en toda la provincia entre las 3 sucursales (Resistencia, Charata, Villa Ángela) y Casa Central (Sáenz Peña) a ser utilizado en la peluquería seleccionada.</li> <br>
 				<li style="font-size: 15px">El premio es intrasferible.</li> <br>
+				<li style="font-size: 15px">Quienes participan en el sorteo autorizan a que, en caso de salir beneficiadas, se publique su foto de perfil en nuestra fanpage.</li><br>
+				<li style="font-size: 15px">Una vez realizado el sorteo y notificadas las beneficiadas por MP de facebook, se esperara por 48Hs su respuesta. En caso de cilencio se procederá a un nuevo sorteo en su reemplazo.</li><br>
 			</ul>
 		</div>
 	</div>
