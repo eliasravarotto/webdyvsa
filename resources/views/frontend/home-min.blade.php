21ba17262d010ac166da5442b11dd68ee7f83cb4
Elias Ravarotto <elias_rvt@hotmail.com>
2019-08-30 11:52:11 -0300

    Cambios varios

--- resources/views/frontend/home-min.blade.php
+++ resources/views/frontend/home-min.blade.php
@@ -138,23 +138,18 @@
     <!-- Carusel Principal MOBILE-->
     <div id="myCarousel" class="carousel slide visible-xs visible-sm"  data-ride="carousel">
       <ol class="carousel-indicators">
-        {{-- <li data-target="#carousel-mobile" data-slide-to="0" class="active"></li> --}}
+        <li data-target="#carousel-mobile" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-mobile" data-slide-to="1"></li>
       </ol>
 
       <div class="carousel-inner">
-        {{-- <div class="item active">
-          <img src="/imagenes/home-min/portada-agosto-mobile.jpg" alt="">
-        </div> --}}
+        <div class="item active">
+          <img src="/imagenes/home-min/portada-usados-sm.png" alt="">
+        </div>
+        <div class="item">
+          <img src="/imagenes/home-min/portada-tu-auto-sm.png" alt="">
+        </div>
       </div>
-
-      <a class="left carousel-control" href="#carousel-mobile" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#carousel-mobile" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-        <span class="sr-only">Next</span>
-      </a>
     </div>
   </section>
 
