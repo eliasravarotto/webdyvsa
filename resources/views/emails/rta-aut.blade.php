9fb5f6f9c1dedb98eff3b31a506476d66e6ae3d2
Elias Ravarotto <elias_rvt@hotmail.com>
2019-09-05 10:11:57 -0300

    Plantilla emails

---
+++ resources/views/emails/rta-aut.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gracias Por Comunicarte Con Nosotros</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+</head>
+<body>
+<div>
+	<h5>Gracias por comunicarte con nosotros.</h5>			
+	<p>Tu mensaje será revisado a la brevedad.</p>
+	<br>
+	<ul class="list-unstyled">
+		<li><b>Derka y Vargas S.A.</b> </li>
+		<li>Concesionario Oficial <span class="text-danger"><b>TOYOTA</b></span></li>
+	</ul>
+	<br>
+	<ul class="list-unstyled">
+		<li>Casa Central Sáenz Peña <b>-</b> Belgrano Nº 872. Tel: (0364) 44 - 20602 / 20549 / 20840</li>
+		<li>Sucursal Resistencia <b>-</b> Ruta Nicolas Avellaneda Km. 11,9. Tel: (0362) 476 484 0/47</li>
+		<li>Sucursal Charata <b>-</b> Ruta 89 Km. 74. Tel: 03735 15 620 255</li>
+		<li>Sucursal Villa Angela <b>-</b> Av. 25 de Mayo Nº 1101. Tel: (03735) 423 200</li>
+	</ul>
+</div>
+</body>
+</html>
