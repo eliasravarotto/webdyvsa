edcc8571c617c56bd710c6a8e19cc2f53b7d70b5
Elias Ravarotto <elias_rvt@hotmail.com>
2019-10-22 16:00:47 -0300

    API NOVEDADES

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    public function getNovedades(Request $request, $categoria = null ){
+
+    	if ($categoria == null) {
+    		$posts = Post::orderBy('created_at', 'DESC')->get();
+    	}else{
+    		$posts = Post::where('tema_id', '!=', 5)
+    						->take($limit)
+    						->orderBy('created_at', 'DESC')
+    						->get();
+    	}
+
+    	return response()->json(['data' => $posts ], 200);
+    }
+}
