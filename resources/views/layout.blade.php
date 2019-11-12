85e5ef1af3282e3f31efd924b2465e026a2aeb57
Elias Ravarotto <elias_rvt@hotmail.com>
2019-11-12 13:21:23 -0300

    Team tpa

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta name="keywords" content="Derka y Vargas, Toyota, Chaco, Hilux, Corolla, Etios, Yaris, SW4, Camry, Prius, RAV4, Land Cruiser, Prado, Posventa, Postventa, Servicios, Mantenimiento Express">
+
+    <meta http-equiv="Cache-control" content="no-cache">
+    <meta http-equiv="Expires" content="-1">
+
     
     @yield('title_and_meta')
-    
     <!--===============================================================================================-->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!--===============================================================================================-->
@@ -238,38 +241,11 @@
   <!--===============================================================================================-->
   <!-- Script -->
   <script type="text/javascript">
+
     $(document).ready(function(){
 
         $('[data-toggle="tooltip"]').tooltip();   
         
-        var $animation_elements = $('.et-waypoint'),
-            $window = $(window);
-     
-        function check_if_in_view() {
-            var window_height = $window.height(),
-                window_top_position = $window.scrollTop(),
-                window_bottom_position = (window_top_position + window_height);
-     
-            $animation_elements.each(function() {
-                var $element = $(this),
-                    element_height = $element.outerHeight(),
-                    element_top_position = $element.offset().top,
-                    element_bottom_position = (element_top_position + element_height);
-     
-                //check to see if this element is within viewport
-                if ((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
-                    $element.addClass('.slide-left');
-                } else {
-                    $element.removeClass('.slide-left-show');
-                }
-            });
-        }
-     
-        $window.on('scroll resize', check_if_in_view);
-
-        window.sr = ScrollReveal();
-        sr.reveal('.thumbnail-lighten', {duration: 1500});
-
         $(".a-cargando").click(function(){
           $(this).addClass('disabled')
                   .text('Cargando...');
