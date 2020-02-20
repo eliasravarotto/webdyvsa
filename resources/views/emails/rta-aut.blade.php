c4bfcb7bfa553b7c9a7d9d6954208d80a8288723
Elias Ravarotto <elias_rvt@hotmail.com>
2020-02-20 12:17:43 -0300

    EmailController + Queue + Listen + redis

--- resources/views/emails/rta-aut.blade.php
+++ resources/views/emails/rta-aut.blade.php
@@ -4,7 +4,7 @@
 	<title>Gracias Por Comunicarte Con Nosotros</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link href="{{ asset('/css/app.css') }}" rel="stylesheet" type="text/css" />
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
