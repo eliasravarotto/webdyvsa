3bf0916b4f12caaee039f9084f3d3c14aa95a703
Elias Ravarotto <elias_rvt@hotmail.com>
2020-03-11 15:20:54 -0300

    Cambio vista lista de precios servicios

--- resources/views/frontend/posventa/index.blade.php
+++ resources/views/frontend/posventa/index.blade.php
@@ -193,7 +193,7 @@ TOYOTA RECALL
 		      	</div>
 			      <div class="row">
 			          @foreach(\App\Helpers\Helper::getPostsPromosDtos(null) as $post)
-			            <div class="col-md-4 col-sm-12">
+			            <div class="col-md-4 col-sm-12 mb-2">
 			              <div class="card d-flex flex-column justify-content-between box-shadow-2">
 			                <div class="container-img" style="background: url('{{$post->imagen_portada}}');"></div> 
 			                  <div class="card-body d-flex flex-column justify-content-end" style="height: 230px;">
