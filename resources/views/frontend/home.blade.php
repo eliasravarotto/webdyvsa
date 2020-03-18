67594a128bd1cf05f56728d1460e193597652821
Elias Ravarotto <elias_rvt@hotmail.com>
2020-03-18 19:14:34 -0300

    Aviso importante

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -26,7 +26,7 @@
   
   <section style="padding: 0px 7px; margin-top: 15px">
     <a href="https://derkayvargas.com/posts/aviso-importante"><img src="{{asset('imagenes/home-min/aviso-importante-min.png')}}" class="img-fluid d-none d-md-block d-lg-block" style="border-radius: 7px;"></a>
-    <a href="https://derkayvargas.com/posts/aviso-importante"><img src="https://derkayvargas.com/imagenes/posts/45/90073382_3170586902953425_1459456777971040256_o.jpg" class="img-fluid d-md-none d-lg-none" style="border-radius: 7px;"></a>
+    <a href="https://derkayvargas.com/posts/aviso-importante"><img src="{{asset('imagenes/home-min/aviso_importante.jpg')}}" class="img-fluid d-md-none d-lg-none" style="border-radius: 7px;"></a>
   </section>
 
   <section class="py-4">
