a02c76fb7fb4337962a1d62174412afd362f5d0a
Elias Ravarotto <elias_rvt@hotmail.com>
2020-05-28 11:34:18 -0300

    Cambios varios

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -16,20 +16,20 @@
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" integrity="sha256-+N4/V/SbAFiW1MPBCXnfnP9QSN3+Keu+NlB+0ev/YKQ=" crossorigin="anonymous" />
    <style type="text/css">
     .swal2-title {
-      font-size: 1em;
+      font-size: 1em !important;
     }
     .swal2-content {
-      font-size: 0.8em;
+      font-size: 0.8em !important;
     }
     .swal2-styled {
-      padding: .125em 0.5em;
+      padding: .125em 0.5em !important;
       font-size: 20px !important;
     }
     .swal2-actions {
-      margin: .7em auto 0;
+      margin: .7em auto 0 !important;
     }
     .swal2-popup {
-      width: 22em;
+      width: 22em !important;
     }
    </style>
 @stop
