ff9691dd87006aa4d3caaf8eebf2143007b47b30
Elias Ravarotto <Elias Ravarotto>
2020-09-29 09:15:12 -0300

    Chat Facebook

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -1,3 +1,29 @@
+<!-- Load Facebook SDK for JavaScript -->
+      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v8.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your Chat Plugin code -->
+      <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="127409800604499"
+  logged_in_greeting="Hola! en qué podemos ayudarte?"
+  logged_out_greeting="Hola! en qué podemos ayudarte?">
+      </div>
+
 <footer>
 	<section class="text-center prefooter">
 		<div class="container">
