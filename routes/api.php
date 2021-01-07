5a60543caa7fd844dc6408e8b408549fa40eb973
Elias Ravarotto <Elias Ravarotto>
2021-01-07 12:08:52 -0300

    Usados API

--- routes/api.php
+++ routes/api.php
@@ -20,10 +20,17 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::middleware('cliente')->get('/novedades','ApiController@getNovedades');
 Route::middleware('cliente')->get('/modelos','ApiController@getModelos');
 Route::middleware('cliente')->get('/tipos-de-servicios','ApiController@getTiposServicios');
-Route::middleware('cliente')->post('/turno-servicio','TurnoServicioController@store');
+// Route::middleware('cliente')->post('/turno-servicio','TurnoServicioController@store');
 Route::middleware('cliente')->get('/sucursales/{sucursales_de?}','ApiController@getSucursales');
 
 Route::get('/entradas','PostController@getPosts')->name('posts');
 Route::get('/entradas/{slug}','PostController@getPost')->name('single_post');
 Route::get('/modelos','ModelosController@getModelos');
 Route::get('/modelos/{slug}','ModelosController@getModelo');
+Route::get('/usados','UsadoController@getUsados');
+Route::post('/message','MessageController@store');
+Route::post('/turno-servicio','TurnoServicioController@store');
+Route::post('/solicitar-test-drive','SolicitudTestDriveController@store');
+Route::get('/team-tpa','ApiController@getTeamTpa');
+Route::get('/plan-de-ahorro/oportunidades/avanzados','TpaAgrupadosController@index');
+Route::get('/plan-de-ahorro/oportunidades/adjudicados','TpaAdjudicadosController@index');
